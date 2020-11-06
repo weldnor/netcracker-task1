5490df3808e8b92845ed1c654e6fb34b40af1d29
Kolesov Anton <kolesov.anton.s@gmail.com>
2020-11-06 15:08:05 +0300

    Refactor code

--- src/main/java/com/weldnor/netcracker/task1/App.java
+++ src/main/java/com/weldnor/netcracker/task1/App.java
@@ -2,7 +2,7 @@ package com.weldnor.netcracker.task1;
 
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println("Hello World!");
     }
 }
--- src/main/java/com/weldnor/netcracker/task1/sorter/Sorter.java
+++ src/main/java/com/weldnor/netcracker/task1/sorter/Sorter.java
@@ -6,18 +6,18 @@ public interface Sorter<T> {
     /**
      * Метод для сортировки части массива по возрастанию.
      *
-     * @param array      массив, часть которого будет отсортирована.
-     * @param comparator критерий, по которому сравниваются элементы.
-     * @param start      начальный индекс.
-     * @param end        конечный индекс.
+     * @param array      массив, часть которого будет отсортирована
+     * @param comparator критерий, по которому сравниваются элементы
+     * @param start      начальный индекс
+     * @param end        конечный индекс
      */
     void sort(T[] array, Comparator<T> comparator, int start, int end);
 
     /**
      * Метод для сортировки массива по возрастанию.
      *
-     * @param array      массив, который будет отсортирован.
-     * @param comparator критерий, по которому сравниваются элементы.
+     * @param array      массив, который будет отсортирован
+     * @param comparator критерий, по которому сравниваются элементы
      */
     default void sort(T[] array, Comparator<T> comparator) {
         if (array.length > 1) {
