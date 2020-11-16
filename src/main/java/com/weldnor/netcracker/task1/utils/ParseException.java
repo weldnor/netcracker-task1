c6723955f58a121a26825ed3ed102c1425622ee2
Kolesov Anton <kolesov.anton.s@gmail.com>
2020-11-16 17:43:34 +0300

    Add ContractCsvLoader, ParseException

---
+++ src/main/java/com/weldnor/netcracker/task1/utils/ParseException.java
@@ -0,0 +1,10 @@
+package com.weldnor.netcracker.task1.utils;
+
+public class ParseException extends Exception {
+    /**
+     * @param message сообщение об ошибке
+     */
+    public ParseException(String message) {
+        super(message);
+    }
+}
